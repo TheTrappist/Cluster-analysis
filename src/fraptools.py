5d6e6e18f30094c964bc7fd258a626bd4bc48503
Vlad Belyy <vlad@walterlab.ucsf.edu>
2019-02-08 15:00:44 -0800

    Improved bleach point finding and added manual trace exclusions

--- src/fraptools.py
+++ src/fraptools.py
@@ -30,6 +30,60 @@ def read_ij_intensity (file):
     
     return intensity_data
 
+def get_traces_from_df_list(df_list, file_names, exclude=None):
+    """Extract singe traces from Extract_two_radii_TrackMate.ijm data.
+    
+    Args:
+        df_list (list of pandas dataframes): List of parsed data from
+            the output file of the "Extract_two_radii_TrackMate.ijm"
+            ImageJ macro.
+        file_names (list of str): List of file names that the traces
+            are coming from
+        exclude (list of tuples): Traces to exclude. Each item in the list
+            is a tuple containing file name and track ID of the trace to
+            be excluded. Defaults to None.
+    
+    Returns:
+        result_df (list of pandas dataframes): One dataframe per trace.
+            Contains all the same columns as df_list; the only difference
+            is that this one is split into smaller single-trace chunks.
+        corr_int (list of numpy arrays): list of one array per trace,
+            containing just the background-corrected intensity values.
+        trace_IDs (list of tuples): list of filenames corresponding 
+            to each trace and trace ID. Same length as result_df.
+        
+    """
+    
+    result_df = []
+    result_np = []
+    trace_IDs = []
+    
+    for df, file_name in zip(df_list, file_names):
+        df.sort_values(by=['track_IDs', 'frames'])
+        
+        # Find all the transitions between tracks by changes in track_ID
+        diff = df['track_IDs'][1:].values - df['track_IDs'][:-1].values
+        track_start_points = np.append([0],(np.flatnonzero(diff)+1))
+        track_slices = np.append(track_start_points, [len(diff)+1])            
+        
+        # break up data into smaller data frames, one per trace
+        for i in range(len(track_slices)-1):
+            curr_df = df[track_slices[i]:track_slices[i+1]]
+            bkgnd_corr_int = curr_df['mean_int_inner']-curr_df['bknd_int']
+            curr_np = bkgnd_corr_int.values
+
+            # save the trace if it's not excluded
+            curr_trace_number = curr_df['track_IDs'].iloc[0]
+            trace_ID = (file_name, curr_trace_number)
+            
+            if trace_ID not in exclude:
+            
+                result_df.append(curr_df)
+                result_np.append(curr_np)   
+                trace_IDs.append(trace_ID)
+        
+    return result_df, result_np, trace_IDs
+
 def correct_background (bkgnd_data, track_coords, track_intensities):
     """Subtract background intensity from track_coords.
     
@@ -88,6 +142,109 @@ def fit_frap(bkgnd_corr_int, frame_interval=1.0, bleach_n_frames=1):
         # Then find mean intensity prior to bleaching
         last_prebleach_frame = bleach_frame - bleach_n_frames
         last_prebleach_frame = max([last_prebleach_frame, 1])
+        y_prebleach = trace_norm[:last_prebleach_frame]
+        int_prebleach = np.mean(y_prebleach)
+        
+        trace_norm = trace_norm / int_prebleach
+        
+        # get subset of data for fitting
+        y_data = trace_norm[bleach_frame:]
+        x_data = np.array(range(len(y_data))) * frame_interval
+        x_whole_trace = np.array(range(len(trace_norm))) * frame_interval
+        
+        # set up fit bounds
+        a_upper = max(y_data)*2
+        if a_upper <= 0:
+            a_upper = 0.001 # catches traces that are fully lower than zero
+        b_upper = 4 / frame_interval
+        fit_bounds = (0, [a_upper, b_upper])
+        
+        # fit recovery curve
+        popt, pcov = curve_fit(frap_fun, x_data, y_data, bounds=fit_bounds)
+        fit_result_y = frap_fun(x_data, *popt)
+        thalf = np.log(2) / popt[1]
+        mobile_fraction = popt[0]
+        
+        # package fit results
+        fit_result_curr = {'popt':popt, 'pcov':pcov, 'thalf':thalf,
+                           'mobile_fraction':mobile_fraction}
+        
+        # package normalized data:
+        t_trunc = x_data + bleach_time
+        norm_d = {'t_trunc':t_trunc, 't_trunc_0':x_data, 'y_trunc':y_data,
+                  't_full':x_whole_trace, 'y_full':trace_norm, 
+                  'fit_curve':fit_result_y}
+        
+        fit_results.append(fit_result_curr)
+        norm_data.append(norm_d)
+        
+    return fit_results, norm_data
+
+def fit_frap_smart(traces, frame_interval=1.0, max_bleach_frames=6):
+    """Fit each trace to a FRAP curve with smart normalization.
+    
+    This function finds the bleach point of the trace using a maximum-slope
+    method and is more robust at identifying the bleach and pre-bleach points
+    than fit_frap in complex or noisy traces.
+    
+    Args:
+        traces (list): List of numpy arrays (one for each track in 
+            track_intensities) storing background-subtracted track intensities
+        frame_interval (float): time, in seconds, between subsequent frames.
+            Defaults to 1.0.
+        max_bleach_frames (int): how many frames can the bleaching pulse last?
+            Defaults to 3.
+
+    Returns:
+        fit_results (list): List of fit parameters and y-values for the fit 
+            curve. One element for each input trace.
+        norm_data (list): Normalized x and y data used for fitting. One element
+            for each input track.            
+    """
+    fit_results = []    
+    norm_data = []
+
+    for trace in traces:
+        
+        # Find the frame where recovery begins.
+        # First, locate the point with the largest negative slope.
+        slopes = trace[1:] - trace[:-1]
+        min_slope = min(slopes)
+        min_slope_index = np.where(slopes == min_slope)[0][0] + 1
+        
+        # Find the local minimum around the minimum slope point:
+        try:
+            local_start_index = min_slope_index - max_bleach_frames
+            local_end_index = min_slope_index + max_bleach_frames
+            trace_local = trace[local_start_index:local_end_index]
+            min_index_local = np.argmin(trace_local)
+            min_index = min_index_local + local_start_index
+        except:
+            print("Good local minimum not found; using minimum slope.")
+            min_index = min_slope_index
+        
+        recovery_start_index = min_index
+        
+        # Step back in time to find where bleaching starts
+        bleach_start_index = min_slope_index
+        f_cur = 0
+        try:
+            while slopes[bleach_start_index] < 0 and f_cur < max_bleach_frames:
+                bleach_start_index = bleach_start_index - 1
+                f_cur = f_cur + 1
+        except:
+            bleach_start_index = min_slope_index
+            print("Bleach start not found")
+            break
+             
+        # This frame is the bleach frame and contains the minimum intensity
+        lowest = trace[recovery_start_index]
+        bleach_frame = recovery_start_index
+        bleach_time = bleach_frame * frame_interval
+        trace_norm = np.array(trace-lowest)
+        
+        # Then find mean intensity prior to bleaching
+        last_prebleach_frame = max([bleach_start_index, 1])
         y_prebleach = trace_norm[:last_prebleach_frame]
         int_prebleach = np.mean(y_prebleach)
         
